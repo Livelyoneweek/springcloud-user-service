5c189ec68a97f8cc0c56a10d1a87dd7318338479
Livelyoneweek <wlvvwhs@naver.com>
2023-03-28 00:02:12 +0900

    user service 등록

---
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.example.userservice.controller;
+
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.service.UserService;
+import com.example.userservice.vo.Greeting;
+import com.example.userservice.vo.RequestUser;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final Greeting greeting;
+    private final UserService userService;
+
+    @GetMapping("/health_check")
+    public String status() {
+        return "It's Working in user-service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return greeting.getMessage();
+    }
+
+    @PostMapping("/users")
+    public String createUser(@RequestBody RequestUser user) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserDto userDto = mapper.map(user, UserDto.class);
+        userService.createUser(userDto);
+
+        return "Create user method is called";
+    }
+}
