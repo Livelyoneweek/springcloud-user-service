a279d857183b57c1dcea3e9036579fbec6b5b852
Livelyoneweek <wlvvwhs@naver.com>
2023-04-11 23:35:48 +0900

    @EnableFeignClients 추가

--- src/main/java/com/example/userservice/UserServiceApplication.java
+++ src/main/java/com/example/userservice/UserServiceApplication.java
@@ -3,12 +3,15 @@ package com.example.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class UserServiceApplication {
 
     public static void main(String[] args) {
@@ -21,6 +24,7 @@ public class UserServiceApplication {
     }
 
     @Bean
+    @LoadBalanced // config 서비스에서 가져오는 url에 마이크로서비스 이름을 넣고 사용하기위해 필요
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
