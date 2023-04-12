ee8b47f2d7e7089a951f6a5aacf81c6d4a9fd0a8
Livelyoneweek <wlvvwhs@naver.com>
2023-04-12 23:12:33 +0900

    FeignErrorDecoder 사용(자동)

--- src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.userservice.service;
 import com.example.userservice.client.OrderServiceClient;
 import com.example.userservice.dto.UserDto;
 import com.example.userservice.entity.UserEntity;
+import com.example.userservice.error.FeignErrorDecoder;
 import com.example.userservice.repository.UserRepository;
 import com.example.userservice.vo.ResponseOrder;
 import feign.FeignException;
@@ -65,13 +66,17 @@ public class UserServiceImpl implements UserService{
 //                });
 //        List<ResponseOrder> orderList = orderListResponse.getBody();
 
+
         // Using a Feign Client + exception handling
-        List<ResponseOrder> orderList = null;
-        try {
-            orderList = orderServiceClient.getOrders(userId);
-        } catch (FeignException ex) {
-            log.error(ex.getMessage());
-        }
+//        List<ResponseOrder> orderList = null;
+//        try {
+//            orderList = orderServiceClient.getOrders(userId);
+//        } catch (FeignException ex) {
+//            log.error(ex.getMessage());
+//        }
+
+        // ErrorDecoder 사용
+        List<ResponseOrder> orderList = orderServiceClient.getOrders(userId);
 
         userDto.setOrders(orderList);
 
